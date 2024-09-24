ea5caa2370778718d4d68422fee72cf5c8759ae5
Luis Grajeda <lgrajeda@msudenver.edu>
2024-09-24 03:03:06 -0600

    SevenTestAndValidations

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -3,32 +3,32 @@ require "test_helper"
 class StudentTest < ActiveSupport::TestCase
   test "Saving student without first name" do
     assert_raises ActiveRecord::RecordInvalid do
-      Student.create!(last_name: "Nikola", school_email: "jokic@msudenver.edu", major: "CS")
+      Student.create!(last_name: "Nikola", school_email: "jokic@msudenver.edu", major: "CS", graduation_date: 2025-05-05)
     end
   end
 
   test "Saving student without last name" do
     assert_raises ActiveRecord::RecordInvalid do 
-      Student.create!(first_name: "Jokic", school_email: "jokic@msudenver.edu", major: "CS")
+      Student.create!(first_name: "Jokic", school_email: "jokic@msudenver.edu", major: "CS", graduation_date: 2025-05-05)
     end
   end
 
   test "Saving student without email" do
     assert_raises ActiveRecord::RecordInvalid do 
-      Student.create!(first_name: "Nikola", last_name: "Jokic", major: "CS")
+      Student.create!(first_name: "Nikola", last_name: "Jokic", major: "CS", graduation_date: 2025-05-05)
     end
   end
 
   test "Saving student without major" do
     assert_raises ActiveRecord::RecordInvalid do 
-      Student.create!(first_name: "Nikola", last_name: "Jokic", school_email: "jokic@msudenver.edu")
+      Student.create!(first_name: "Nikola", last_name: "Jokic", school_email: "jokic@msudenver.edu", graduation_date: 2025-05-05)
     end
   end
 
    test "Saving student with non-unique email" do
     student = students(:one)
     assert_raises ActiveRecord::RecordInvalid do 
-      Student.create!(first_name: "Nikola", last_name: "Jokic", school_email: "jokic@msudenver.edu", major: "CS")
+      Student.create!(first_name: "Nikola", last_name: "Jokic", school_email: "jokic@msudenver.edu", major: "CS", graduation_date: 2025-05-05)
     end
   end
 
@@ -37,13 +37,13 @@ class StudentTest < ActiveSupport::TestCase
       Student.create!(first_name: "Nikola", last_name: "Jokic", school_email: "nikola@msudenver.edu", major: "CS")
     end
   end
-=begin
-  test "" do
+
+  test "Saving first_name with a digit" do
     assert_raises ActiveRecord::RecordInvalid do 
-      Student.create!(first_name: "Jokic", school_email: "jokic@msudenver.edu", major: "CS")
+      Student.create!(first_name: "Nikola1", last_name: "Jokic", school_email: "nikola@msudenver.edu", major: "CS", graduation_date: 2025-05-05)
     end
   end
-
+=begin
   test "" do
     assert_raises ActiveRecord::RecordInvalid do 
       Student.create!(first_name: "Jokic", school_email: "jokic@msudenver.edu", major: "CS")
