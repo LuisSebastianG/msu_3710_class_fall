6fdd2c2edef9f5d09f8e7b40fad4d6b3a42df019
Luis Grajeda <lgrajeda@msudenver.edu>
2024-09-25 02:07:39 -0600

    ProfilePicture

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,24 @@
 class Student < ApplicationRecord
+    has_one_attached :profile_picture
+
     validates :first_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
     validates :last_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
     validates :school_email, presence: true, uniqueness: true
     validates :major, presence: true
     validates :graduation_date, presence: true
+    validate :acceptable_image
+
+    def acceptable_image
+        return unless profile_picture.attached?
+
+        unless profile_picture.blob.byte_size <= 1.megabyte
+            errors.add(:profile_picture, "is too big")
+        end
+
+        acceptable_types = ["image/jpeg", "image/png"]
+        unless acceptable_types.include?(profile_picture.content_type)
+            errors.add(:profile_picture, "must be a JPEG or PNG")
+        end
+    end
+
 end
